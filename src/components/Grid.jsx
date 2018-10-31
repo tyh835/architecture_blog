e2262a1c596513095a7e03969edc54a81d62da8e
Tony Han <itony9401@live.com>
2018-10-31 00:03:56 -0700

    small formatting

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -47,7 +47,7 @@ const Grid = ({ data }) => {
       <div className="d__subtitle">
         Bassicarella Architectes, Beijing, China Temple
       </div>
-      
+
       <div className="e__image">
         <Img
           fluid={data.image5.childImageSharp.fluid}
